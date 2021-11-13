78fb2e38bd9782a682761f0440c34fbc7029083a
Bhavik Dholakiya <bhavikdholakiya6@gmail.com>
2021-11-13 22:08:58 +0530

    13 nov 2021 10:08pm

---
+++ static/javascript/profile.js
@@ -0,0 +1,16 @@
+function clicked1(){
+    var temp=document.getElementById('email');
+    temp.disabled=false;
+}
+function clicked2(){
+    var temp=document.getElementById('name');
+    temp.disabled=false;
+}
+function clicked3(){
+    var temp=document.getElementById('password');
+    temp.disabled=false;
+}
+function clicked4(){
+    var temp=document.getElementById('age');
+    temp.disabled=false;
+}
